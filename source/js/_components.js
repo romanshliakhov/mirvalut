6c383e0cd3d097b625768b0e76fa617d6163f4b3
Roman <romanshliakhov@gmail.com>
2023-02-05 15:55:57 +0200

    add all sections content

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,10 +1,7 @@
-import './components/sliders.js';
 import './components/select.js';
-import './components/burger.js';
 import './components/spollers.js';
 import './components/dropdown.js';
 import './components/tabs.js';
-import './components/modals.js';
 
 
 
